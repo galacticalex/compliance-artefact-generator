cef100995ca678207d9868e552c6018e06c9a583
Alexander Murphy <alexander.murphy@orcro.co.uk>
2022-06-14 15:54:12 +0100

    generic text for each file included

--- generate-artefacts.R
+++ generate-artefacts.R
@@ -5,6 +5,25 @@
 
 
 ##
+#
+# Table of Contents:
+#
+# 27. Environment Variables and Settings
+#
+# 46. Error Message Definitions
+#
+# 82. File Content Defintions
+#
+# 110. Function Definitions
+#
+# 135. Program Logic
+#
+# 154. Environment Cleanup
+#
+##
+
+
+##
 # Environment Variables and Settings Start ----
 ##
 
@@ -12,9 +31,9 @@ warnStatus = getOption("warn")
 
 options(warn = -1)
 
-artefactFileNames = c("Licensing_Overview.txt", 
-                      "Licensing_Appendix_1.txt", 
-                      "Licensing_Appendix_2.txt")
+artefactFileNames = c("Licensing_Overview.md",
+                      "Licensing_Appendix_1.md",
+                      "Licensing_Appendix_2.md")
 
 args = commandArgs(trailingOnly = T)
 
@@ -24,63 +43,91 @@ args = commandArgs(trailingOnly = T)
 
 
 ##
-# Function Definitions Start ----
-##
-
-outputFile = function(fileText, fileName) {
-  write(x = fileText, file = fileName)
-}
-
-generateArtefacts = function(L) {
-  
-  data = L
-  
-  print(dim(data))
-  
-  outputFile("hi", artefactFileNames[1])
-  outputFile("hi", artefactFileNames[2])
-  outputFile("hi", artefactFileNames[3])
-}
-
-##
-# Function Definitions Start ----
-##
-
-
-##
-# Error Messages Start ----
+# Error Message Definitions Start ----
 ##
 
 errorHeader = "\n\x1b[31;1mError.\x1b[0m See details:\n\n"
 errorFooter = "\n\nFor support, contact \x1b[31mteam@orcro.co.uk\x1b[0m\n\n"
 
 #1
-errorArgsNumber = paste0(errorHeader, 
-                         "The command should have a single argument.", 
-                         "The command should look like this:\n\n\t", 
-                         "Rscript generate-artefacts.R \x1b[36mLicenceList\x1b[0m.csv\n\n", 
-                         "Replace \x1b[36mLicenceList\x1b[0m with the name of ", 
-                         "your licence list file.", 
+errorArgsNumber = paste0(errorHeader,
+                         "The command should have a single argument.",
+                         "The command should look like this:\n\n\t",
+                         "Rscript generate-artefacts.R \x1b[36mLicenceList\x1b[0m.csv\n\n",
+                         "Replace \x1b[36mLicenceList\x1b[0m with the name of ",
+                         "your licence list file.",
                          errorFooter)
 #2
-errorFileExists = paste0(errorHeader, 
-                         "An artefact output file(s) already exists. ", 
-                         "To prevent data loss, move the existing file(s)", 
-                         " out of this directory.", 
+errorFileExists = paste0(errorHeader,
+                         "An artefact output file(s) already exists. ",
+                         "To prevent data loss, move the existing file(s)",
+                         " out of this directory.",
                          errorFooter)
 
 #3
-errorInputMissing = paste(errorHeader, 
+errorInputMissing = paste(errorHeader,
                           "The specified licence list file does not exist.",
-                          errorFooter) 
+                          errorFooter)
 
 #all
-errorMessages = c(errorArgsNumber, 
-                  errorFileExists, 
+errorMessages = c(errorArgsNumber,
+                  errorFileExists,
                   errorInputMissing)
 
 ##
-# Error Messages End ----
+# Error Message Definitions End ----
+##
+
+##
+# File Content Start ----
+##
+
+overviewText = paste0("# Overview\n\nThis software contains a number of open source components. For a summary, see \n",
+                      "below. For the relevant licence texts, see Appendix A. For the relevant notices \n",
+                      "and attributions etc., see Appendix B.\n\n",
+                      "The following summary contains information about the main author(s) and/or\n",
+                      "copyright holder(s) of each component and the overall package licence. For full \n",
+                      "copyright notices etc., see Appendix B.\n\n",
+                      "Not all components listed may be incorporated in, or may necessarily have \n",
+                      "generated derivative works which are incorporated in the firmware, but were \n",
+                      "used during the build process.\n\n",
+                      "Where a range of dates is given after a copyright notice, this should be taken \n",
+                      "to imply that copyright is asserted for every year within that range, inclusive \n",
+                      "of the years stated.\n\n",
+                      "# Component List")
+
+appendixAText = "# Appendix A: Licence Texts"
+appendixBText = "# Appendix B: Notices and Attribution"
+
+genericFileContent = c(overviewText, appendixAText, appendixBText)
+
+##
+# File Content End ----
+##
+
+
+##
+# Function Definitions Start ----
+##
+
+outputFile = function(fileText, fileName) {
+    write(x = fileText, file = fileName)
+}
+
+generateArtefacts = function(L) {
+
+    data = L
+
+    # test
+    print(dim(data))
+
+    outputFile(genericFileContent[1], artefactFileNames[1])
+    outputFile(genericFileContent[2], artefactFileNames[2])
+    outputFile(genericFileContent[3], artefactFileNames[3])
+}
+
+##
+# Function Definitions Start ----
 ##
 
 
@@ -89,13 +136,13 @@ errorMessages = c(errorArgsNumber,
 ##
 
 if (length(args) != 1) {
-  cat(errorMessages[1])
+    cat(errorMessages[1])
 } else if (artefactFileNames %in% dir()) {
-  cat(errorMessages[2])
+    cat(errorMessages[2])
 } else if (file.exists(args)) {
-  generateArtefacts(read.csv(args))
+    generateArtefacts(read.csv(args))
 } else {
-  cat(errorMessages[3])
+    cat(errorMessages[3])
 }
 
 ##
@@ -114,3 +161,4 @@ rm(args, artefactFileNames, warnStatus, generateArtefacts, outputFile)
 ##
 # Environment Cleanup End ----
 ##
+
