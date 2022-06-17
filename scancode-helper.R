6f476dafc9aa0f884f32748554a3c3274da64835
Alexander Murphy <alexander.murphy@orcro.co.uk>
2022-06-17 13:33:49 +0100

    scancode run command

---
+++ scancode-helper.R
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2022 Orcro Limited. <team@orcro.co.uk>
+# 
+# SPDX-License-Identifier: Apache-2.0
+
+# short wrapper around scancode command
+
+system("~/scancode-toolkit/scancode -clp -n 18 --csv ScancodeOutput.csv sources/")
